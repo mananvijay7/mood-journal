ed2902f0ca098084f01c268d189dd05f5b5e21c0
mananvijay7 <vijayvargiya.m@northeastern.edu>
2024-01-14 16:22:45 -0500

    Landing page design

--- mood/app/page.tsx
+++ mood/app/page.tsx
@@ -1,7 +1,18 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div>
+        <h1 className="text-6xl">Welcome to mood.ai</h1>
+        <p className="text-3xl text-white/60">Become closer to the Self</p>
+        <p className="text-white/50 mb-4">Journal your thoughts, track your moods.</p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-white text-black px-4 py-2 rounded-lg text-xl">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
